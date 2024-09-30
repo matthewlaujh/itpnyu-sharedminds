44f328ac0a39102dfd4206fd1298b490b9eb489e
Matthew Lau <75940464+matthewlaujh@users.noreply.github.com>
2024-09-30 18:22:13 -0400

    firebase config

---
+++ week4/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBADrPwczOJivVXboRy99x3TANkT_rhods",
+  authDomain: "itpnyu-b6b1d.firebaseapp.com",
+  projectId: "itpnyu-b6b1d",
+  storageBucket: "itpnyu-b6b1d.appspot.com",
+  messagingSenderId: "407753427861",
+  appId: "1:407753427861:web:645e06ecb1385278b1449c",
+  measurementId: "G-WTSVM6ZS2M",
+}
